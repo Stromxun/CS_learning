5ddbb32c49219d0feef882290406d949c2004c1c
Stromxun <102493146+Stromxun@users.noreply.github.com>
2023-08-16 09:31:28 +0800

    Delete CSAPP/Chapter_3 directory

--- CSAPP/Chapter_3/mstore.c
+++
@@ -1,8 +0,0 @@
-// #include<stdio.h>
-
-long mult2(long, long);
-
-void multstore(long x, long y, long *dest){
-    long t = mult2(x, y);
-    *dest = t;
-}
--- CSAPP/Chapter_3/scale.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-long scale(long x, long y, long z){
-    long t = x + 4 * y + 12 * z;
-    return t;
-}
